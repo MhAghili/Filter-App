a8579294a35cae000b95879141dd9ae95eae114f
Mohammad Aghili <mhaghili1381@yahoo.com>
2023-07-12 14:16:15 +0330

    fix scroll bug

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <div className="p-2 bg-secondary-subtle rounded-3 h-100 overflow-auto ">
         <Filter />
       </div>
-      <div className="p-2 h-100 w-100 text-center ">
+      <div className="p-2 h-100 w-100 text-center overflow-auto">
         <UsersContent />
       </div>
     </Card>
